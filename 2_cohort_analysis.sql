24210dfcbbe9c2bc78806866af2999dd3ca3258d
Seth <work.lenkovits@gmail.com>
2025-04-21 08:16:36 +0100

    Added View SQL

--- 2_cohort_analysis.sql
+++ 2_cohort_analysis.sql
@@ -1,3 +1,30 @@
+-- This SQL script creates a view for cohort analysis based on customer purchases.
+CREATE OR REPLACE VIEW public.cohort_analysis
+AS WITH customer_revenue AS (
+         SELECT s.customerkey,
+            s.orderdate,
+            sum(s.quantity::double precision * s.netprice * s.exchangerate) AS total_net_revenue,
+            count(s.orderkey) AS num_orders,
+            c.countryfull,
+            c.age,
+            c.givenname,
+            c.surname
+           FROM sales s
+             LEFT JOIN customer c ON c.customerkey = s.customerkey
+          GROUP BY c.countryfull, c.age, c.givenname, c.surname, s.customerkey, s.orderdate
+        )
+ SELECT customerkey,
+    orderdate,
+    total_net_revenue,
+    num_orders,
+    countryfull,
+    age,
+    givenname,
+    surname,
+    min(orderdate) OVER (PARTITION BY customerkey) AS first_purchase_date,
+    EXTRACT(year FROM min(orderdate) OVER (PARTITION BY customerkey)) AS cohort_year
+   FROM customer_revenue cr;
+   
 SELECT
 	cohort_year,
 	count(DISTINCT customerkey) AS total_customers,
